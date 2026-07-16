9bcd73dd7fe7945bdf1e57d405c899baad6ec451
gogitakupatadze <kupatadze2000@outlook.com>
2026-07-16 20:24:02 +0400

    refactor: add ThermoHammer.Api.Models namespace to all EF Core migration designer files and snapshot

--- ThermoHammer.Api/ThermoHammer.Api/Migrations/20260715094140_hammertypetolist.Designer.cs
+++ ThermoHammer.Api/ThermoHammer.Api/Migrations/20260715094140_hammertypetolist.Designer.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using ThermoHammer.Api.Models;
+
 
 #nullable disable
 
